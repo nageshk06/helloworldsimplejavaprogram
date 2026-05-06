a11dbdf7e9f8cdbc8976838f84f7d1d2272e369f
nageshk06 <43815644+nageshk06@users.noreply.github.com>
2026-05-06 16:31:22 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,9 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello batch43 team with  Nagesh, Yogesh , Sachoin");
+     System.out.println("Hello batch43 team with  Nagesh, Yogesh , Sachin");
       System.out.println("Hello Batch batch43 WITH FOR STUDENTS RUNNING in front of me and Nagesh in making CI automated vikas teach devops CI proecss");
          System.out.println("I am making changes in GIT. Should poll changes");
+        System.out.println("I am making changes in jenkins job. It should run");
+    }  
     }  
 } 

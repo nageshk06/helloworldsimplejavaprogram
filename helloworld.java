63cdc9b0688a21643d2a0946369bafccb5e15592
nageshk06 <43815644+nageshk06@users.noreply.github.com>
2026-05-06 16:45:23 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,4 +1,4 @@
-class Simple{  
+class helloworld{  
     public static void main(String args[]){  
      System.out.println("Hello batch43 team with  Nagesh, Yogesh , Sachin");
       System.out.println("Hello Batch batch43 WITH FOR STUDENTS RUNNING in front of me and Nagesh in making CI automated vikas teach devops CI proecss");
